eaa634fd3407f609482a3a2804f3786926608b06
ivan catacata <catacata.ivan@gmail.com>
2022-10-24 23:21:08 -0300

    update

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#6df6a1",
+  secondary: "#00cede",
+  tertiary: "#ffff96",
+  background: "#002230",
+  text: "#212121",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#808080",
+};
